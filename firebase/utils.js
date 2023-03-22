58f12953b82c2a8d643cd49c00a1c48ff4628748
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2023-03-22 12:24:05 -0400

    Update utils.js
    Cc

--- firebase/utils.js
+++ firebase/utils.js
@@ -1,9 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { app } from './config'
-import { onAuthStateChanged, getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut  } from "firebase/auth";
-import { getDatabase, ref, onValue, set, update, child, get, query, remove, limitToFirst, limitToLast, orderByValue, orderByChild, endAt , equalTo} from "firebase/database";
-import { getList, getIndexStorage} from './storage'
-import { getDate, getDayMonthYear, getMonthAndYear} from '../utils/Utils'
+import { onAuthStateChanged, getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getDatabase, ref, onValue, set, update, child, get, query, remove, limitToFirst, limitToLast, orderByValue, orderByChild, endAt, equalTo } from "firebase/database";
+import { getList, getIndexStorage } from './storage'
+import { getDate, getDayMonthYear, getMonthAndYear } from '../utils/Utils'
 
 
 
@@ -17,7 +17,7 @@ function onAuth(setUserProfile, setUserData, postsIMG, setUserPostsIMG, setUserD
     } else {
       setUserProfile(user)
     }
-    
+
     getMonthAndYear(setUserMonthAndYear)
     getDayMonthYear(setUserDayMonthYear)
   });
@@ -25,41 +25,41 @@ function onAuth(setUserProfile, setUserData, postsIMG, setUserPostsIMG, setUserD
 
 // ---------------------------Login, Sign Up and Sign In------------------------------------
 
-function signUpWithEmail (email, password, setUserSuccess) {
+function signUpWithEmail(email, password, setUserSuccess) {
   createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    setUserSuccess('SignUpError')
-    // ..
-  });
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      setUserSuccess('SignUpError')
+      // ..
+    });
 }
 
-function signInWithEmail (email, password, setUserSuccess) {
+function signInWithEmail(email, password, setUserSuccess) {
   signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    setUserSuccess('Verify')
-  });
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      setUserSuccess('Verify')
+    });
 }
 
-function handleSignOut () {
+function handleSignOut() {
   signOut(auth).then(() => {
-  // Sign-out successful.
-}).catch((error) => {
-  // An error happened.
-});
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+  });
 }
 
 // -------------------------------Firebase Realtime Database------------------------------------
@@ -87,46 +87,61 @@ function getIndexData(setUserData, date) {
 
 
 
-    arr2.map((i) => { 
+  arr2.map((i) => {
     get(query(ref(db, i)))
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        let snap = snapshot.val()
-        setUserData(allData)
-        allData = { ...allData, [i]: snap }
-      }
-    });
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          let snap = snapshot.val()
+          setUserData(allData)
+          allData = { ...allData, [i]: snap }
+        }
+      });
   });
 
 
-  arr3.map((i) => { 
+  arr3.map((i) => {
     get(query(ref(db, i)))
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        let snap = snapshot.val()
-        setUserData(allData)
-        allData = { ...allData, [i]: snap }
-      }
-    });
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          let snap = snapshot.val()
+          setUserData(allData)
+          allData = { ...allData, [i]: snap }
+        }
+      });
   });
 
+  const months = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
 
   arr.map((i) => {
     // get(query(ref(db, `${i}/Posts`), limitToLast(5), orderByChild('fecha'), endAt(date) ))
 
-    get(query(ref(db, `${i}/Posts`), limitToLast(5) ))
+    get(query(ref(db, `${i}/Posts`), limitToLast(5)))
       .then((snapshot) => {
 
         if (snapshot.exists()) {
 
-          let snap = snapshot.val()
-          allData = {
-            ...allData, [i]: {
-              Posts: snap,
-              Templates: 'hello'
-            }
-          }
-       setUserData(allData)
+          // ${new Date(date).getDate()}-${months[new Date(date).getMonth()]}-${new Date(date).getFullYear()}
+
+          get(query(ref(db, `${i}/Templates/${(`${new Date(date).getDate()}-${months[new Date(date).getMonth()]}-${new Date(date).getFullYear()}`).toString()}`)))
+            .then((snapshotTemp) => {
+              console.log(snapshotTemp.val())
+              // console.log(new Date(date).getDate())
+              // console.log(months[new Date(date).getMonth()])
+              // console.log(new Date(date).getFullYear())
+
+              let snap = snapshot.val()
+              allData = {
+                ...allData, [i]: {
+                  Posts: snap,
+                  Templates: {
+                    [`${new Date(date).getDate()}-${months[new Date(date).getMonth()]}-${new Date(date).getFullYear()}`] : snapshotTemp.val()
+                  }
+                }
+              }
+              setUserData(allData)
+
+
+            })
         }
       })
   });
@@ -139,9 +154,9 @@ function getSpecificData(rute, specificData, setUserSpecificData) {
   get(child(dbRef, `${rute}`)).then((snapshot) => {
     if (snapshot.exists()) {
       let snap = snapshot.val()
-      setUserSpecificData({...specificData, [key] : snap})
+      setUserSpecificData({ ...specificData, [key]: snap })
     } else {
-      setUserSpecificData({...specificData, [key] : {nota: 'en redaccion'}})
+      setUserSpecificData({ ...specificData, [key]: { nota: 'en redaccion' } })
     }
   }).catch((error) => {
     console.error(error);
@@ -157,15 +172,16 @@ function writeUserData(ruteDB, object, setUserSuccess, detail) {
     .catch(() => '')
 }
 
- function removeData(ruteDB, setUserData, setUserSuccess) {
- remove(ref(db, ruteDB))
- .then(() => {
-  getIndexData(setUserData)
-  setUserSuccess('save')
-})
- .catch(() => setUserSuccess('repeat'));
+function removeData(ruteDB, setUserData, setUserSuccess) {
+  remove(ref(db, ruteDB))
+    .then(() => {
+      getIndexData(setUserData)
+      setUserSuccess('save')
+    })
+    .catch(() => setUserSuccess('repeat'));
 }
-export {app, onAuth, signUpWithEmail, signInWithEmail, handleSignOut, getIndexData, getSpecificData, writeUserData, removeData, }
+export { app, onAuth, signUpWithEmail, signInWithEmail, handleSignOut, getIndexData, getSpecificData, writeUserData, removeData, }
+
 
 
 
